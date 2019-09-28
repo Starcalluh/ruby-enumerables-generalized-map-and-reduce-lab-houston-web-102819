cc6d9c18c331bc5419f8f0eb0eabc85353a8e4a7
George <starcalluh@live.com>
2019-09-28 05:38:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,9 +10,18 @@ end
 
 def add_numbers(num1, num2)
   total = num1 + num2 
+  yeild(total)
+  total
 end
 
 
 add_numbers(1,2) do |t|
   puts "The total is #(t)"
 end
+
+add_numbers(3,5) do |x|
+  puts "that adds up to #(x)"
+end
+
+
+map reduce
