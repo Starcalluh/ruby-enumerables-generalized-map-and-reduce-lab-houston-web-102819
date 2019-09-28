65e61622bd29384f651f300184b9a9e9d6407957
George <starcalluh@live.com>
2019-09-28 05:35:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -5,5 +5,14 @@ end
 
 
 
-[1,2,3].map{|num| num + 1
-}
+[1,2,3].map{|num| num + 1}
+
+
+def add_numbers(num1, num2)
+  total = num1 + num2 
+end
+
+
+add_numbers(1,2) do |t|
+  puts "The total is #(t)"
+end
