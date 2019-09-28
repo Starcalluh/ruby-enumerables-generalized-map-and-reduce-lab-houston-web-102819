acb28f3fea7a5df3e3151007203ac4fc4544f70e
George <starcalluh@live.com>
2019-09-28 05:42:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,27 +1,4 @@
 # Your Code Here
-[1,2,3].map do |num|
-  num + 1 
-end
-
-
-
-[1,2,3].map{|num| num + 1}
-
-
-def add_numbers(num1, num2)
-  total = num1 + num2 
-  yeild(total)
-  total
-end
-
-
-add_numbers(1,2) do |t|
-  puts "The total is #(t)"
-end
-
-add_numbers(3,5) do |x|
-  puts "that adds up to #(x)"
-end
 
 
 map reduce
